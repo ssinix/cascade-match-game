ec77d05d35e01c1cd9c461891aaa7a04d03368ee
azra <azraat32@gmail.com>
2025-02-06 18:07:44 +0300

    Add loop to validate file input until successful

--- cascade_match.cpp
+++ cascade_match.cpp
@@ -8,6 +8,23 @@
 using namespace std;
 
 int main() {
+    string filename;
+    cout << "Please enter the file name: ";
+    cin >> filename;
+
+    ifstream input;
+    input.open(filename);
+
+    while (input.fail()) {
+        cout << "The file couldn't be opened." << endl;
+        cout << "Please enter a valid file name: ";
+        cin >> filename;
+        input.open(filename);
+    }
+
+    //cout << "Successfully opened.";
+
+    string line;
 
     return 0;
 }
